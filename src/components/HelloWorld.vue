977890f1b88d618f88135ad42e8b561f0dd87dfb
DESKTOP-C1T53C2\Lenovo <limaijie1011@sina.com>
2019-12-20 15:32:12 +0800

    1221

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,7 +10,28 @@
       <li>
         <a ref="saveSvg" href="javascript:" title="download as SVG image">SVG image</a>
       </li>
+      <li>
+        <el-button type="success" @click="saveXmlFile">保存</el-button>
+      </li>
     </ul>
+    <el-dialog title="弹窗标题" :visible.sync="dialogFormVisible">
+      <el-form :model="form">
+        <!-- <el-form-item label="活动名称" :label-width="formLabelWidth">
+          <el-input v-model="form.name" autocomplete="off"></el-input>
+        </el-form-item>-->
+        <el-form-item label="选择人员：" :label-width="formLabelWidth">
+          <el-select v-model="form.name" placeholder="请选择活动区域">
+            <el-option label="总经理" value="总经理"></el-option>
+            <el-option label="人事部经理" value="人事部经理"></el-option>
+            <el-option label="技术部经理" value="技术部经理"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogSave">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -18,13 +39,25 @@
 // 引入相关的依赖
 // import BpmnViewer from 'bpmn-js'
 import BpmnModeler from "bpmn-js/lib/Modeler";
-import propertiesPanelModule from "bpmn-js-properties-panel";
+// import propertiesPanelModule from "bpmn-js-properties-panel";
 import propertiesProviderModule from "bpmn-js-properties-panel/lib/provider/camunda";
 import camundaModdleDescriptor from "camunda-bpmn-moddle/resources/camunda";
 
 export default {
   data() {
     return {
+      dialogFormVisible: false,
+      form: {
+        name: "",
+        region: "",
+        date1: "",
+        date2: "",
+        delivery: false,
+        type: [],
+        resource: "",
+        desc: ""
+      },
+      formLabelWidth: "120px",
       // bpmn建模器
       bpmnModeler: null,
       container: null,
@@ -41,7 +74,7 @@ export default {
     <bpmn2:startEvent id="StartEvent_1" name="开始">
       <bpmn2:outgoing>SequenceFlow_0z01mg5</bpmn2:outgoing>
     </bpmn2:startEvent>
-    <bpmn2:task id="Task_0r1jhgy" name="请假">
+    <bpmn2:task id="Task_0r1jhgy" name="请假申请">
       <bpmn2:incoming>SequenceFlow_0z01mg5</bpmn2:incoming>
       <bpmn2:outgoing>SequenceFlow_1i99umy</bpmn2:outgoing>
     </bpmn2:task>
@@ -52,84 +85,82 @@ export default {
       <bpmn2:outgoing>SequenceFlow_0s7xgeq</bpmn2:outgoing>
     </bpmn2:exclusiveGateway>
     <bpmn2:sequenceFlow id="SequenceFlow_1i99umy" sourceRef="Task_0r1jhgy" targetRef="ExclusiveGateway_1300i73" />
-    <bpmn2:task id="Task_1vypdv5" name="总经理">
+    <bpmn2:task id="Task_1vypdv5" name="总部审批">
       <bpmn2:incoming>SequenceFlow_02wn0ed</bpmn2:incoming>
       <bpmn2:outgoing>SequenceFlow_0erlkl4</bpmn2:outgoing>
     </bpmn2:task>
     <bpmn2:sequenceFlow id="SequenceFlow_02wn0ed" name="大于十天" sourceRef="ExclusiveGateway_1300i73" targetRef="Task_1vypdv5" />
-    <bpmn2:task id="Task_1470v5j" name="经理">
-      <bpmn2:incoming>SequenceFlow_0s7xgeq</bpmn2:incoming>
-      <bpmn2:outgoing>SequenceFlow_0kgndvb</bpmn2:outgoing>
-    </bpmn2:task>
     <bpmn2:sequenceFlow id="SequenceFlow_0s7xgeq" name="小于十天" sourceRef="ExclusiveGateway_1300i73" targetRef="Task_1470v5j" />
     <bpmn2:endEvent id="EndEvent_0lupkw8" name="结束">
-      <bpmn2:incoming>SequenceFlow_0kgndvb</bpmn2:incoming>
-      <bpmn2:incoming>SequenceFlow_0erlkl4</bpmn2:incoming>
+      <bpmn2:incoming>SequenceFlow_1sr2dbl</bpmn2:incoming>
     </bpmn2:endEvent>
-    <bpmn2:sequenceFlow id="SequenceFlow_0kgndvb" sourceRef="Task_1470v5j" targetRef="EndEvent_0lupkw8" />
-    <bpmn2:sequenceFlow id="SequenceFlow_0erlkl4" sourceRef="Task_1vypdv5" targetRef="EndEvent_0lupkw8" />
+    <bpmn2:sequenceFlow id="SequenceFlow_0erlkl4" sourceRef="Task_1vypdv5" targetRef="Task_1470v5j" />
+    <bpmn2:userTask id="Task_1470v5j" name="经理审批">
+      <bpmn2:incoming>SequenceFlow_0s7xgeq</bpmn2:incoming>
+      <bpmn2:incoming>SequenceFlow_0erlkl4</bpmn2:incoming>
+      <bpmn2:outgoing>SequenceFlow_1sr2dbl</bpmn2:outgoing>
+    </bpmn2:userTask>
+    <bpmn2:sequenceFlow id="SequenceFlow_1sr2dbl" sourceRef="Task_1470v5j" targetRef="EndEvent_0lupkw8" />
   </bpmn2:process>
   <bpmndi:BPMNDiagram id="BPMNDiagram_1">
     <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="Process_1">
       <bpmndi:BPMNShape id="_BPMNShape_StartEvent_2" bpmnElement="StartEvent_1">
-        <dc:Bounds x="412" y="240" width="36" height="36" />
+        <dc:Bounds x="422" y="342" width="36" height="36" />
         <bpmndi:BPMNLabel>
-          <dc:Bounds x="419" y="283" width="22" height="14" />
+          <dc:Bounds x="429" y="385" width="22" height="14" />
         </bpmndi:BPMNLabel>
       </bpmndi:BPMNShape>
       <bpmndi:BPMNShape id="Task_0r1jhgy_di" bpmnElement="Task_0r1jhgy">
-        <dc:Bounds x="498" y="218" width="100" height="80" />
+        <dc:Bounds x="590" y="320" width="100" height="80" />
       </bpmndi:BPMNShape>
       <bpmndi:BPMNEdge id="SequenceFlow_0z01mg5_di" bpmnElement="SequenceFlow_0z01mg5">
-        <di:waypoint x="448" y="258" />
-        <di:waypoint x="498" y="258" />
+        <di:waypoint x="458" y="360" />
+        <di:waypoint x="590" y="360" />
       </bpmndi:BPMNEdge>
       <bpmndi:BPMNShape id="ExclusiveGateway_1300i73_di" bpmnElement="ExclusiveGateway_1300i73" isMarkerVisible="true">
-        <dc:Bounds x="648" y="233" width="50" height="50" />
+        <dc:Bounds x="785" y="335" width="50" height="50" />
         <bpmndi:BPMNLabel>
           <dc:Bounds x="650" y="203" width="46" height="14" />
         </bpmndi:BPMNLabel>
       </bpmndi:BPMNShape>
       <bpmndi:BPMNEdge id="SequenceFlow_1i99umy_di" bpmnElement="SequenceFlow_1i99umy">
-        <di:waypoint x="598" y="258" />
-        <di:waypoint x="648" y="258" />
+        <di:waypoint x="690" y="360" />
+        <di:waypoint x="785" y="360" />
       </bpmndi:BPMNEdge>
       <bpmndi:BPMNShape id="Task_1vypdv5_di" bpmnElement="Task_1vypdv5">
-        <dc:Bounds x="748" y="218" width="100" height="80" />
+        <dc:Bounds x="1040" y="130" width="100" height="80" />
       </bpmndi:BPMNShape>
       <bpmndi:BPMNEdge id="SequenceFlow_02wn0ed_di" bpmnElement="SequenceFlow_02wn0ed">
-        <di:waypoint x="698" y="258" />
-        <di:waypoint x="748" y="258" />
+        <di:waypoint x="810" y="335" />
+        <di:waypoint x="810" y="170" />
+        <di:waypoint x="1040" y="170" />
         <bpmndi:BPMNLabel>
-          <dc:Bounds x="701" y="240" width="44" height="14" />
+          <dc:Bounds x="903" y="152" width="44" height="14" />
         </bpmndi:BPMNLabel>
       </bpmndi:BPMNEdge>
-      <bpmndi:BPMNShape id="Task_1470v5j_di" bpmnElement="Task_1470v5j">
-        <dc:Bounds x="748" y="328" width="100" height="80" />
-      </bpmndi:BPMNShape>
       <bpmndi:BPMNEdge id="SequenceFlow_0s7xgeq_di" bpmnElement="SequenceFlow_0s7xgeq">
-        <di:waypoint x="673" y="283" />
-        <di:waypoint x="673" y="368" />
-        <di:waypoint x="748" y="368" />
+        <di:waypoint x="835" y="360" />
+        <di:waypoint x="1040" y="360" />
         <bpmndi:BPMNLabel>
-          <dc:Bounds x="666" y="323" width="44" height="14" />
+          <dc:Bounds x="903" y="373" width="44" height="14" />
         </bpmndi:BPMNLabel>
       </bpmndi:BPMNEdge>
       <bpmndi:BPMNShape id="EndEvent_0lupkw8_di" bpmnElement="EndEvent_0lupkw8">
-        <dc:Bounds x="998" y="350" width="36" height="36" />
+        <dc:Bounds x="1282" y="342" width="36" height="36" />
         <bpmndi:BPMNLabel>
-          <dc:Bounds x="1005" y="393" width="22" height="14" />
+          <dc:Bounds x="1289" y="385" width="22" height="14" />
         </bpmndi:BPMNLabel>
       </bpmndi:BPMNShape>
-      <bpmndi:BPMNEdge id="SequenceFlow_0kgndvb_di" bpmnElement="SequenceFlow_0kgndvb">
-        <di:waypoint x="848" y="368" />
-        <di:waypoint x="998" y="368" />
-      </bpmndi:BPMNEdge>
       <bpmndi:BPMNEdge id="SequenceFlow_0erlkl4_di" bpmnElement="SequenceFlow_0erlkl4">
-        <di:waypoint x="848" y="258" />
-        <di:waypoint x="923" y="258" />
-        <di:waypoint x="923" y="368" />
-        <di:waypoint x="998" y="368" />
+        <di:waypoint x="1090" y="210" />
+        <di:waypoint x="1090" y="320" />
+      </bpmndi:BPMNEdge>
+      <bpmndi:BPMNShape id="UserTask_0xqe5og_di" bpmnElement="Task_1470v5j">
+        <dc:Bounds x="1040" y="320" width="100" height="80" />
+      </bpmndi:BPMNShape>
+      <bpmndi:BPMNEdge id="SequenceFlow_1sr2dbl_di" bpmnElement="SequenceFlow_1sr2dbl">
+        <di:waypoint x="1140" y="360" />
+        <di:waypoint x="1282" y="360" />
       </bpmndi:BPMNEdge>
     </bpmndi:BPMNPlane>
   </bpmndi:BPMNDiagram>
@@ -157,8 +188,6 @@ export default {
     // 当图发生改变的时候会调用这个函数，这个data就是图的xml
     setEncoded(link, name, data) {
       // 把xml转换为URI，下载要用到的
-      console.log(666666666666666);
-      console.log(data);
       const encodedData = encodeURIComponent(data);
       // 获取到图的xml，保存就是把这个xml提交给后台
       this.xmlStr = data;
@@ -168,7 +197,61 @@ export default {
         link.href = "data:application/bpmn20-xml;charset=UTF-8," + encodedData;
         link.download = name;
       }
+    },
+    //保存整个XML文件
+    saveXmlFile() {
+      console.log(this.xmlStr);
+    },
+    //弹窗保存
+    dialogSave() {
+      let name = this.form.name;
+      const modeling = this.bpmnModeler.get("modeling");
+      modeling.updateLabel(this.element, name);
+      this.dialogFormVisible = false;
+    },
+    handleModeler() {
+      // 监听节点选择变化
+      this.bpmnModeler.on("selection.changed", e => {
+        const element = e.newSelection[0];
+        this.element = element;
+        console.log(this.element);
+        if (!element) return;
+        if (element.businessObject.$type === "bpmn:UserTask") {
+          this.dialogFormVisible = true;
+        }
+
+        // this.$message({
+        //   message: "当前内容：" + element.businessObject.name,
+        //   type: "success"
+        // });
+        // this.form = {
+        //   ...element.businessObject,
+        //   ...element.businessObject.$attrs
+        // };
+        // if (this.form.userType === "candidateUsers") {
+        //   this.form["candidateUsers"] =
+        //     this.form["candidateUsers"].split(",") || [];
+        // }
+        // console.log(this.form)
+      });
+
+      //  监听节点属性变化
+      this.bpmnModeler.on("element.changed", e => {
+        const { element } = e;
+        if (!element) return;
+        //  新增节点需要更新回属性面板
+        // if (element.id === this.form.id) {
+        //   this.form.name = element.businessObject.name;
+        //   this.form = { ...this.form };
+        // }
+      });
     }
+    // 属性面板名称，更新回流程节点
+    // nameChange(name) {
+    //   console.log(name);
+    //   const modeling = this.bpmnModeler.get("modeling");
+    //   modeling.updateLabel(this.element, name);
+    // }
   },
   mounted() {
     // 获取到属性ref为“content”的dom节点
@@ -185,13 +268,13 @@ export default {
       },
       additionalModules: [
         // 左边工具栏以及节点
-        propertiesProviderModule,
+        propertiesProviderModule
         // 右边的工具栏
-        propertiesPanelModule
-      ],
-      moddleExtensions: {
-        camunda: camundaModdleDescriptor
-      }
+        // propertiesPanelModule
+      ]
+      // moddleExtensions: {
+      //   camunda: camundaModdleDescriptor
+      // }
     });
 
     // 下载画图
@@ -211,6 +294,7 @@ export default {
     });
 
     this.createNewDiagram(this.bpmnModeler);
+    this.handleModeler();
   }
 };
 </script>
@@ -232,6 +316,9 @@ export default {
 .canvas {
   width: 100%;
   height: 100%;
+}
+.canvas > div > a {
+  display: none;
 }
 .panel {
   position: absolute;
